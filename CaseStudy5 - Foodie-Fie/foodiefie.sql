e8851324a4978ef80afcad284c2b6c87dab72ed9
mukaruernest <mukaruernesto@gmail.com>
2021-08-06 21:18:18 +0300

    foodie-fie

---
+++ CaseStudy5 - Foodie-Fie/foodiefie.sql
@@ -0,0 +1,34 @@
+/*
+    How many customers has Foodie-Fi ever had?
+    What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+    What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
+    What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
+    How many customers have churned straight after their initial free trial - what percentage is this rounded to the nearest whole number?
+    What is the number and percentage of customer plans after their initial free trial?
+    What is the customer count and percentage breakdown of all 5 plan_name values at 2020-12-31?
+    How many customers have upgraded to an annual plan in 2020?
+    How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
+    Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
+    How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
+*/
+-- 1.  How many customers has Foodie-Fi ever had?
+ SELECT COUNT(DISTINCT customer_id) AS TotalCustomers
+ FROM subscriptions; 
+ 
+ -- 2 What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+with cte AS 
+ (SELECT MONTH(start_date) AS monthly, COUNT(plan_id) AS monthcount 
+ FROM subscriptions
+ GROUP BY MONTH(start_date)
+ ORDER BY monthly)
+ SELECT monthly, STDDEV(monthcount) AS Distribution
+ FROM cte
+ GROUP BY monthly;
+ 
+ -- 3 What plan start_date values occur after the year 2020 for our dataset? 
+ -- Show the breakdown by count of events for each plan_name
+
+ 
+
+
+ 
