d817086f58249a222686c74ae3899637adf6e34e
mukaruernest <mukaruernesto@gmail.com>
2021-08-16 22:50:28 +0300

    mysql to postgre

--- CaseStudy3 - Foodie-Fie/foodiefie.sql
+++ CaseStudy3 - Foodie-Fie/foodiefie.sql
@@ -11,23 +11,22 @@
 */
 -- 1.  How many customers has Foodie-Fi ever had?
  SELECT COUNT(DISTINCT customer_id) AS TotalCustomers
- FROM subscriptions; 
+ FROM foodie_fi.subscriptions; 
  
  -- 2 What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
-with cte AS 
- (SELECT MONTH(start_date) AS monthly, COUNT(plan_id) AS monthcount 
- FROM subscriptions
- GROUP BY MONTH(start_date)
- ORDER BY monthly)
- SELECT monthly, STDDEV(monthcount) AS Distribution
- FROM cte
- GROUP BY monthly;
+SELECT
+  DATE_TRUNC('WEEK', start_date) AS month_start,
+  COUNT(*) AS trial_customers
+FROM foodie_fi.subscriptions
+WHERE plan_id = 0
+GROUP BY month_start
+ORDER BY month_start;
  
  -- 3 What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
 SELECT p.plan_name, COUNT(*) AS TotalCount
-FROM subscriptions s
+FROM foodie_fi.subscriptions s
 INNER JOIN plans p ON s.plan_id = p.plan_id
-WHERE YEAR(start_date) > 2020  
+WHERE DATE_PART('MONTH', start_date) > 2020  
 GROUP BY  plan_name;
 
 -- 4 What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
